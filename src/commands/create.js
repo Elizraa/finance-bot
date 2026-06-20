c681eda471ebb22e0825223b935dc88c753c56d2
elizraa <azrielwah@gmail.com>
2026-06-20 15:36:47 +0700

    feat: added unit test for balance, create, and toggle_categories

--- src/commands/create.js
+++ src/commands/create.js
@@ -2,10 +2,7 @@ export default function register(bot, { log, dbHelpers }) {
   bot.command('create', (ctx) => {
     const userId = ctx.from.id;
     log.user(userId, '/create command');
-    console.log(
-      '!dbHelpers.hasApiKey(userId) :>> ',
-      !dbHelpers.hasApiKey(userId),
-    );
+
     if (!dbHelpers.hasApiKey(userId)) {
       return ctx.reply(
         '❌ API Key belum diatur.\n\n' +
